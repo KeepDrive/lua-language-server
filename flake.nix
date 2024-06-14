665d10d4151f1d042beb668e7e572e20977a87b1
KeepDrive <ipad2kirillo@gmail.com>
2024-06-14 21:13:27 +0000

    added flake for building

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "lua-ls flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            ninja
+          ];
+        };
+      }
+    );
+}
